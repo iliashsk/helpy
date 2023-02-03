af1157f3e4ca7c0c390ca68f21fbb11970b05859
iliash <iliash.sk462@gmail.com>
2023-02-03 21:27:11 +0530

    helpy

--- server.js
+++ server.js
@@ -38,13 +38,13 @@ app.use(errorHandler)
 
 if ( process.env.NODE_ENV ==="production"){
 
-    app.use(express.static(path.resolve(__dirname,  'frontend', 'build')));
+    app.use(express.static(path.resolve(__dirname,  '/frontend', '/build')));
 
   
 
     app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname,  'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname,  '/frontend', '/build', '/index.html'));
 
     })
 }
