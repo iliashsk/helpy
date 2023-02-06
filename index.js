ac235edcabca4098f13d37160f832d42286de7f9
iliash <iliash.sk462@gmail.com>
2023-02-06 11:03:05 +0530

    helpy

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 // Serve Frontend
 if (process.env.NODE_ENV ==="production"){
 
-    app.use(express.static("frontend/public"));
+    app.use(express.static("frontend/build"));
 
   
 
     app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 
     })
 }
